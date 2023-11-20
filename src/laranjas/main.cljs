e5459f71ed7edfa0eb83a3fdc257a1c411a0a199
Looke Z <luke@Lookes-MacBook-Pro.local>
2023-11-20 22:34:57 +0000

    webGL

--- src/laranjas/main.cljs
+++ src/laranjas/main.cljs
@@ -1,180 +1,127 @@
 (ns laranjas.main
   (:require [goog.object :as g]))
 
-(def game-state (atom {:stop false
-                       :p-systems []}))
+(def game-state (atom {}))
 
 (def c (js/document.getElementById "app"))
 
-(def ctx (.getContext c "2d"))
+(def gl (.getContext c "webgl"))
 
-(def dimensions (atom {:x 100
-                       :y 100}))
+(def dimensions
+  (atom {:x 100 :y 100}))
 
-(def mouse (atom {:x 0
-                  :y 0}))
-
-(defn dim-frac [dim frac]
-  (-> @dimensions dim (* frac)))
-
-(defn abs->frac [dim v]
-  (/ v (-> @dimensions dim)))
-
-(def frac-x (partial dim-frac :x))
-(def frac-y (partial dim-frac :y))
-
-(defn clear []
-  (g/set ctx "fillStyle" "black")
-  (.fillRect ctx 0 0 (frac-x 1) (frac-y 1)))
-
-(defn circle [center-x center-y r fill]
-  (.beginPath ctx)
-  (.arc ctx
-        (frac-x center-x)
-        (frac-y center-y)
-        r
-        0
-        (* 2 js/Math.PI))
-  (.closePath ctx)
-  (g/set ctx "fillStyle" fill)
-  (.fill ctx))
-
-(defn rand-from-range [r-min r-max]
-  (+ r-min (* (rand) (- r-max r-min))))
-
-(defn make-particle
-  [{:keys [life-min life-max x y
-           v-max
-           r-min r-max]}]
-  {:life (rand-from-range life-max life-min)
-   :position [x y]
-   :v [(rand-from-range (- v-max) v-max)
-       (rand-from-range (- v-max) v-max)]
-   :r (rand-from-range r-min r-max)
-   :color [(+ 200 (rand-int 55))
-           (+ 100 (rand-int 100)) 0 (rand)]
-   :spin (rand 0.03)})
-
-(defn make-p-system
-  [{:keys [n-parts]
-    :as args}]
-  (let [parts (->> #(make-particle args)
-                   repeatedly
-                   (take n-parts))]
-    (assoc args :ps parts)))
-
-(defn rotate-vector
-  [[vx vy] theta]
-  [(+ (* vx (js/Math.cos theta))
-      (* vy (- (js/Math.sin theta))))
-   (+ (* vy (js/Math.cos theta))
-      (* vx (js/Math.sin theta)))])
-
-
-(defn update-particle
-  [{:keys [r-decay] :as p-system}
-   {:keys [position v life r spin]
-    :as p}]
-  (let [[px py] position
-        [vx vy] (rotate-vector v spin)
-        new-vx (if (<= 0 px 1) vx (- vx))
-        new-vy (if (<= 0 py 1) vy (- vy))
-        new-x (+ px new-vx)
-        new-y (+ py new-vy)]
-
-    (if (<= life 0)
-      (make-particle p-system)
-      (assoc p
-             :position
-             [new-x new-y]
-             :v [new-vx new-vy]
-             :life (dec life)
-             :r (* r-decay r)))))
-
-(defn update-p-system [p-system]
-  (let [update-p-fn (partial update-particle p-system)]
-    (-> p-system
-        (update :ps #(mapv update-p-fn %))
-        (assoc :x (:x @mouse)
-               :y (:y @mouse)))))
-
-(defn update-game []
-  (swap! game-state update :p-systems
-         #(mapv update-p-system %))
-  (swap! game-state update :p-systems
-         #(mapv update-p-system %)))
-
-(defn draw []
-  (clear)
-  (doseq [p-sys (:p-systems @game-state)
-          {:keys [position r color]} (:ps p-sys)]
-    (let [[col-r col-g col-b col-a] color
-          [x y] position]
-      (circle x y r (str "rgba(" col-r
-                         ", "    col-g
-                         ", "    col-b
-                         ", "    col-a ")")))))
-
-(defn reset-mouse [e]
-  (reset! mouse
-          {:x (abs->frac
-               :x (- (g/get e "clientX")
-                     (g/get c "offsetLeft")))
-           :y (abs->frac
-               :y (- (g/get e "clientY")
-                     (g/get c "offsetTop")))}))
-
-(defn reset-dimensions [_]
+(defn reset-dimensions []
   (let [width js/window.innerWidth
         height js/window.innerHeight]
     (g/set c "width" width)
     (g/set c "height" height)
     (reset! dimensions {:x width
-                        :y height}))
-  true)
+                        :y height})
+    (.viewport gl 0 0 width height)))
 
-(defn step [ts]
-  (let [interval (- ts (or (@game-state :ts) ts))]
-    (swap! game-state
-           assoc
-           :ts ts
-           :interval interval))
-  (update-game)
-  (draw)
-  (when-not (:stop @game-state)
-    (js/window.requestAnimationFrame step)))
+(defn clear []
+  (.clearColor gl 0 0 0 1)
+  (.clear gl (g/get gl "COLOR_BUFFER_BIT")))
+
+(defn create-shader [shader-type source]
+  (let [shader (.createShader gl shader-type)]
+    (.shaderSource gl shader source)
+    (.compileShader gl shader)
+    (js/console.log
+     (str "compile shader: "
+          (.getShaderParameter
+           gl shader (g/get gl "COMPILE_STATUS"))))
+    shader))
+
+(def frag-shader-source
+  "
+precision mediump float;
+
+void main() {
+  gl_FragColor = vec4(1, 0, 0.5, 1);
+}
+")
+
+(def vert-shader-source
+  "
+attribute vec4 a_position; 
+
+void main() {
+  gl_Position = a_position;
+}
+")
+
+(defn frag-shader []
+  (create-shader (g/get gl "FRAGMENT_SHADER")
+                 frag-shader-source))
+
+(defn vert-shader []
+  (create-shader (g/get gl "VERTEX_SHADER")
+                 vert-shader-source))
+
+(defn create-prog [vert frag]
+  (let [program (.createProgram gl)]
+    (.attachShader gl program vert)
+    (.attachShader gl program frag)
+    (.linkProgram gl program)
+    (js/console.log
+     (str "link status: "
+          (.getProgramParameter gl program
+                                (g/get gl "LINK_STATUS"))))
+    program))
+
+(defn clean-up []
+  (.deleteProgram gl (:prog @game-state))
+  (.deleteShader  gl (:vs @game-state))
+  (.deleteShader  gl (:fs @game-state)))
 
 (defn -main []
-  (reset-dimensions nil)
   (.addEventListener js/window
                      "resize"
                      reset-dimensions)
-  (.addEventListener c
-                     "mousemove"
-                     reset-mouse)
-  (swap! game-state
-         update
-         :p-systems
-         conj
-         (make-p-system {:x 0.5
-                         :y 0.5
-                         :r-min 1
-                         :r-max 30
-                         :n-parts 1000
-                         :life-max 500
-                         :life-min 100
-                         :v-max 0.003
-                         :r-decay 0.998}))
+  (let [vs (vert-shader)
+        fs (frag-shader)
+        prog (create-prog vs fs)
+        _ (reset! game-state {:vs vs :fs fs :prog prog})
+        pos-attr-loc (.getAttribLocation gl
+                                         prog
+                                         "a_position")
+        pos-buff (.createBuffer gl)
+        _ (.bindBuffer gl
+                       (g/get gl "ARRAY_BUFFER")
+                       pos-buff)
+        points [0 0 0 0.5 0.7 0]
+        _ (.bufferData gl
+                       (g/get gl "ARRAY_BUFFER")
+                       (js/Float32Array. points)
+                       (g/get gl "STATIC_DRAW"))]
+    (reset-dimensions)
+    (clear)
+    (.useProgram gl prog)
+    (.enableVertexAttribArray gl pos-attr-loc)
+    (.bindBuffer gl
+                 (g/get gl "ARRAY_BUFFER")
+                 pos-buff)
+    (let [size 2
+          dtype (g/get gl "FLOAT")
+          normalize? false
+          stride 0
+          offset 0]
+      (.vertexAttribPointer gl
+                            pos-attr-loc
+                            size
+                            dtype
+                            normalize?
+                            stride
+                            offset))
+    (let [prim-type (g/get gl "TRIANGLES")
+          offset 0
+          cnt 3]
+      (.drawArrays gl prim-type offset cnt))))
 
-  (js/window.requestAnimationFrame step))
+(comment
+  (clean-up)
 
-(defn reset-game []
-  (reset! game-state {:stop true
-                      :p-systems []})
-  nil)
+  (-main)
 
-(defn freeze []
-  (swap! game-state assoc :stop true)
-  nil)
-
-(-main)
+  ,)
